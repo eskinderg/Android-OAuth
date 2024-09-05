3366d5b52538448c0df6368ae1c0c720045fa970
Eskinder <eskinderget@gmail.com>
2024-09-05 14:32:40 +0300

    context change

--- app/src/main/java/app/mynote/auth/UserManager.java
+++ app/src/main/java/app/mynote/auth/UserManager.java
@@ -6,9 +6,9 @@ public class UserManager {
 
     public static User getUser(Context context) {
         User user = new User();
-        String id =MyNote.getContext().getSharedPreferences("prefs", Context.MODE_PRIVATE).getString("sub", null);
-        String email = MyNote.getContext().getSharedPreferences("prefs", Context.MODE_PRIVATE).getString("email", null);
-        String givenName = MyNote.getContext().getSharedPreferences("prefs", Context.MODE_PRIVATE).getString("given_name", null);
+        String id =context.getSharedPreferences("prefs", Context.MODE_PRIVATE).getString("sub", null);
+        String email = context.getSharedPreferences("prefs", Context.MODE_PRIVATE).getString("email", null);
+        String givenName = context.getSharedPreferences("prefs", Context.MODE_PRIVATE).getString("given_name", null);
 
         if(id == null || email == null || givenName == null)
             return null;
